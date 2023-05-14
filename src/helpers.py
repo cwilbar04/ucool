f7d5effd3fb70655eb131d11afb751c59b603653
Christopher Wilbar <cwilbar@alumni.nd.edu>
2023-05-14 13:26:38 +0000

    add helper file to parse params

---
+++ src/helpers.py
@@ -0,0 +1,11 @@
+import yaml
+
+def read_params(config_path):
+    """
+    read parameters from the params.yaml file
+    input: params.yaml location
+    output: parameters as dictionary
+    """
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
