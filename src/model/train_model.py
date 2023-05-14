51f71f7108af863785a7aad2abe517f212e10618
Christopher Wilbar <cwilbar@alumni.nd.edu>
2023-05-14 14:44:10 +0000

    add new functionality to train model

--- src/model/train_model.py
+++ src/model/train_model.py
@@ -4,10 +4,10 @@ import pickle
 import mlflow
 import mlflow.sklearn
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score,recall_score,accuracy_score,precision_score,confusion_matrix,classification_report
 
-from preprocess import process_csv
+from data.preprocess import process_csv
 
 def get_feat_and_target(df,features,target):
     """
@@ -19,29 +19,47 @@ def get_feat_and_target(df,features,target):
     y=df[[target]]
     return X,y
 
-def train_model(df, config):
-    train_data_path = config
-    test_data_path
+def train_model(model_config):
 
-    # Split train and test features 
-    train = pd.read_csv(train_data_path, sep=",")
-    test = pd.read_csv(test_data_path, sep=",")
-    train_x,train_y=get_feat_and_target(train,target)
-    test_x,test_y=get_feat_and_target(test,target)
+    # Load train and test data from config
+    train = pd.read_csv(model_config['train_data_path'], sep=",")
+    test = pd.read_csv(model_config['test_data_path'], sep=",")
 
-    X = data[['Length of First Name', 'Distance']]
-    y = data['Coolness']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Split in to feature and target dataframes based on config
+    train_x,train_y=get_feat_and_target(train, model_config['features'], model_config['target'])
+    test_x,test_y=get_feat_and_target(test, model_config['features'], model_config['target'])
 
     # Build and train the machine learning model
-    model = RandomForestClassifier(max_depth=max_depth,n_estimators=n_estimators)
-    model.fit(X_train, y_train)
+    model = RandomForestClassifier(max_depth=model_config['max_depth'],n_estimators=model_config['n_estimators'])
+    model.fit(train_x, test_y)
 
-    return model, X_test, y_test
+    return model, test_x, test_y
 
-def main(input_csv_path, mlflow_tracking_uri, output_model_path):
-    # Read the CSV file and preprocess the data
-    data = process_csv(input_csv_path)
+def evaluate_model(y_test,predictions,avg_method):
+    accuracy = accuracy_score(y_test, predictions)
+    precision = precision_score(y_test, predictions, average=avg_method)
+    recall = recall_score(y_test, predictions, average=avg_method)
+    f1score = f1_score(y_test, predictions, average=avg_method)
+    target_names = ['not cool','cool']
+    print("Classification report")
+    print("---------------------","\n")
+    print(classification_report(y_test, predictions,target_names=target_names),"\n")
+    print("Confusion Matrix")
+    print("---------------------","\n")
+    print(confusion_matrix(y_test, predictions),"\n")
+
+    print("Accuracy Measures")
+    print("---------------------","\n")
+    print("Accuracy: ", accuracy)
+    print("Precision: ", precision)
+    print("Recall: ", recall)
+    print("F1 Score: ", f1score)
+
+    return accuracy,precision,recall,f1score
+
+def main(config,input_csv_path, mlflow_tracking_uri, output_model_path):
+    # Establish model config dictionary
+    model_config = config['model_config']
 
     # Set the MLflow tracking URI
     mlflow.set_tracking_uri(mlflow_tracking_uri)
@@ -51,22 +69,18 @@ def main(input_csv_path, mlflow_tracking_uri, output_model_path):
 
     with mlflow.start_run():
         # Track the parameters
-        mlflow.log_param("csv_file_path", input_csv_path)
-        mlflow.log_param("output_model_path", output_model_path)
-        mlflow.log_param("mlflow_tracking_uri", mlflow_tracking_uri)
+        mlflow.log_param("model_config", model_config)
 
         # Train the model
-        model, X_test, y_test = train_model(data)
+        model, X_test, y_test = train_model(model_config)
 
         # Track the model artifacts
         mlflow.sklearn.log_model(model, "coolness_model")
 
         # Evaluate the model
         test_predictions = model.predict(X_test)
-        mlflow.log_metric("test_rmse", metrics.mean_squared_error(y_test, test_predictions))
-                          
-    #Save the model locally
-    save_model(model, output_model_path)
+        evaluate_model(y_test,test_predictions,model_config['avg_method'])
+        mlflow.log_metric("test_rmse", metrics.mean_squared_error(y_test, test_predictions)
 
 if __name__ == '__main__':
     import argparse
@@ -77,11 +91,5 @@ if __name__ == '__main__':
     parsed_args = args.parse_args()
 
     config = read_params(parsed_args.config)
-    parser = argparse.ArgumentParser(description='Data preprocessing and machine learning training script')
-    parser.add_argument('config', type=str, help='Path to the CSV file')
-    parser.add_argument('mlflow_tracking_uri', type=str, help='MLflow tracking URI')
-    parser.add_argument('model_filepath', type=str, help='Path to save the trained model as a pickle file')
 
-    args = parser.parse_args()
-
-    main(args.csv_file, args.mlflow_tracking_uri, args.model_filepath)
+    main(config)
