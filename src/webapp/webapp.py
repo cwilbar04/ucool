7a650e9e8bba3987b7f9e829223e567f18222f76
Christopher Wilbar <cwilbar@alumni.nd.edu>
2023-05-15 15:06:25 +0000

    Re-work to use mlflow directly

--- src/webapp/webapp.py
+++ src/webapp/webapp.py
@@ -1,48 +1,45 @@
+import pandas as pd
+import os
 from flask import Flask, render_template, request
-import csv
-from io import StringIO
-import pickle
-import sys
+import mlflow.pyfunc
 
-from model_generator.preprocess import process_csv
-
-# from model_generator.preprocess import process_csv
-
+from data.preprocess import create_features
+from helpers import read_params
 
 app = Flask(__name__)
 
+app.config['CONFIG_FILE'] = os.environ.get('CONFIG_FILE', 'params.yml')
+config = read_params(app.config['CONFIG_FILE'])
+mlflow_config = config['model']['mlflow_config']
+
+# Set the MLflow tracking URI
+mlflow.set_tracking_uri(mlflow_config['remote_server_uri'])
 # Load the trained model
-model = None
-with open('./src/model/random_forest.pkl', 'rb') as file:
-    model = pickle.load(file)
+model = mlflow.pyfunc.load_model("models:/is_you_cool_classifier/Staging")
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         # Get the input text from the form
-        text1 = request.form['text1']
-        text2 = request.form['text2']
+        first_name = request.form['text1']
+        last_initial = request.form['text2']
 
-        # Create an in-memory CSV file
-        csv_data = StringIO()
-        csv_writer = csv.writer(csv_data)
-        csv_writer.writerow(['First Name', 'Last Name Initial'])
-        csv_writer.writerow([text1, text2])
-        csv_data.seek(0)
+        # Create a dataframe from the text input
+        input_df=pd.DataFrame({'First Name':[first_name], 'Last Name Initial':[last_initial]})
 
-        # Process the in-memory CSV
-        df = process_csv(csv_data)
+        # Add the features to the dataframe
+        features_df = create_features(input_df)
 
         # Retrieve the processed values
-        length_of_first_name = df.at[0, 'Length of First Name']
-        distance = df.at[0, 'Distance']
+        # length_of_first_name = df.at[0, 'Length of First Name']
+        # distance = df.at[0, 'Distance']
 
         # Make a prediction using the preprocessed data
-        score = model.predict([[length_of_first_name, distance]])
+        score = model.predict(features_df)
 
         return render_template('result.html', score=score[0])
     else:
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
