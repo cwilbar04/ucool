2d1ac343e8fb29e96ea2aecc25016d5dc1580f7c
Christopher Wilbar <cwilbar@alumni.nd.edu>
2023-05-15 21:09:37 +0000

    Add formatting and scoring function to webapp

--- src/model/score.py
+++ src/model/score.py
@@ -1,11 +1,50 @@
 import mlflow
 import pandas as pd
 
-logged_model = 'runs:/113e3aceb54545a19b29b7f29e0ed8df/coolness_model'
+from data.preprocess import create_features
 
-# Load model as a PyFuncModel.
-loaded_model = mlflow.pyfunc.load_model(logged_model)
+def score_input(config, input_df, run_type):
+    '''
+    Use registered mlflow model in stage based on passed run_type argument to generate score for a features_df
+    '''
+    # Generate features df from inputs
+    features_df = create_features(input_df)
 
-# Predict on a Pandas DataFrame.
+    # Get mlflow_config
+    mlflow_config = config['model']['mlflow_config']
 
-loaded_model.predict(pd.DataFrame(data))
+    # Get model name and type of model to grab
+    model_name = mlflow_config['production_config']['register_model_name']
+    model_stage = 'Production' if run_type == 'PROD' else 'Staging'
+
+    # Set the MLflow tracking URI
+    mlflow.set_tracking_uri(mlflow_config['remote_server_uri'])
+
+    # Load the trained model
+    model = mlflow.pyfunc.load_model(f"models:/{model_name}/{model_stage}")
+
+    # Predict on a Pandas DataFrame.
+    prediction = model.predict(pd.DataFrame(features_df))
+
+    # Set meaning of prediction
+    prediction_meaning = f'The model predicted {prediction} which means '
+    prediction_meaning = prediction_meaning + 'you is cool' if prediction == 1 else prediction_meaning + 'you is not cool'
+
+    return prediction, prediction_meaning
+
+if __name__ == '__main__':
+    import argparse
+    from helpers import read_params
+
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yml")
+    args.add_argument("--run_type", default="DEV")
+    args.add_argument("--first_name")
+    args.add_argument("--last_initial")
+    parsed_args = args.parse_args()
+
+    args_df=pd.DataFrame({'First Name':[parsed_args.first_name], 'Last Name Initial':[parsed_args.last_initial]})
+    parsed_config = read_params(parsed_args.config)
+
+    score, meaning = score_input(parsed_config, args_df, parsed_args.run_type)
+    print(score, meaning)
--- src/webapp/webapp.py
+++ src/webapp/webapp.py
@@ -1,21 +1,15 @@
 import pandas as pd
 import os
-from flask import Flask, render_template, request
-import mlflow.pyfunc
+from flask import Flask, render_template, request, redirect
 
-from data.preprocess import create_features
+from model.score import score_input
 from helpers import read_params
 
 app = Flask(__name__)
 
 app.config['CONFIG_FILE'] = os.environ.get('CONFIG_FILE', 'params.yml')
+app.config['RUN_TYPE'] = os.environ.get('RUN_TYPE', 'DEV')
 config = read_params(app.config['CONFIG_FILE'])
-mlflow_config = config['model']['mlflow_config']
-
-# Set the MLflow tracking URI
-mlflow.set_tracking_uri(mlflow_config['remote_server_uri'])
-# Load the trained model
-model = mlflow.pyfunc.load_model("models:/is_you_cool_classifier/Staging")
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -27,17 +21,9 @@ def home():
         # Create a dataframe from the text input
         input_df=pd.DataFrame({'First Name':[first_name], 'Last Name Initial':[last_initial]})
 
-        # Add the features to the dataframe
-        features_df = create_features(input_df)
+        _, score_meaning = score_input(config, input_df, app.config['RUN_TYPE'])
 
-        # Retrieve the processed values
-        # length_of_first_name = df.at[0, 'Length of First Name']
-        # distance = df.at[0, 'Distance']
-
-        # Make a prediction using the preprocessed data
-        score = model.predict(features_df)
-
-        return render_template('result.html', score=score[0])
+        return render_template('result.html', score_meaning=score_meaning)
     else:
         return render_template('index.html')
 
